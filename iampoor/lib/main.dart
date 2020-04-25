524f21c2fffe8c87a2f14487530b68bba49248c3
jordan.ellis.petersen@gmail.com <jordan.ellis.petersen@gmail.com>
2020-04-25 09:08:39 -0500

    added iampoor

---
+++ iampoor/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('I am poor'),
+        ),
+        
+
+      ),
+
+
+    ),
+  );
+
+
+
+
+
+
+
+
+
+}
